dad0b4b721d37332639183e869cb0dad6b5cb6ac
Ben Lau <xbenlau@gmail.com>
2017-10-14 23:47:40 +0800

    runner.js: Read from data/ folder

--- runner.js
+++ runner.js
@@ -6,14 +6,13 @@ var Hubdb = require("hubdb");
 const path = require("path");
 var shell = require("shelljs");
 
-console.log(process.cwd() + "/*.json");
+console.log(__dirname + "/data/*.json");
 var content = shell.ls(process.cwd() + "/*.json").map((file) => { 
     return JSON.parse(shell.cat(file).toString());
 });
 
 for (var i in content) {
     (function(suite) {
-        console.log(suite.name, suite);
         describe(suite.name, function() {
             for (var j in suite.tests) {
                 (function (test) {
@@ -25,48 +24,3 @@ for (var i in content) {
         });
     })(content[i]);
 }
-
-/*
-var db = Hubdb({
- token: '',
- username: 'benlau',
- repo: 'qtbugstatus',
- branch: 'db'
-});
-
-before(function() {
-    
-    var promise = new Promise(function (resolve, reject) {
-        
-        db.list(function(err, res) {
-            console.log("loaded");
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            for (var i in res) {
-                (function(suite) {
-                    console.log(suite.name, suite);
-                    describe(suite.name, function() {
-                        for (var j in suite.tests) {
-                            (function (test) {
-                                it(test.name, function() {
-                                   expect(test.pass).to.equal(true); 
-                                });                                
-                            })(suite.tests[j]);
-                        }                        
-                    });
-                })(res[i].data);
-            }
-            
-            resolve();
-        });
-    });
-
-    return promise;
-});
-
-it("dummy", function() {
-});
-*/
