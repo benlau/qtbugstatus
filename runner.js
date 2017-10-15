41f63b96b2b5f0d7674ad824b93252ee499d6841
Ben Lau <xbenlau@gmail.com>
2017-10-15 13:23:13 +0800

    strip non-printable unicode char

--- runner.js
+++ runner.js
@@ -2,12 +2,15 @@ require("mocha-allure-reporter");
 
 const { expect,assert } = require("chai");
 var Promise = require('promise');
-var Hubdb = require("hubdb");
 const path = require("path");
 var shell = require("shelljs");
 
 var content = shell.ls(__dirname + "/data/*.json").map((file) => { 
-    return JSON.parse(shell.cat(file).toString());
+    var txt =  shell.cat(file).toString();
+    txt = txt.replace(/[\x00-\x09\x0B-\x0C\x0E-\x1F\x7F-\x9F]/g, '');
+    console.log(file, txt);
+
+    return JSON.parse(txt);
 });
 
 for (var i in content) {
